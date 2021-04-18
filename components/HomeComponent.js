274dc1d5cb1afbb2c71ba375105f7d64d37987c0
thommgriffiths <chomas.g@hotmail.com>
2021-04-18 00:12:08 -0300

    Register tab , image picking. Not working

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -67,7 +67,8 @@ class Home extends Component {
         {
           toValue: 8,
           duration: 8000,
-          easing: Easing.linear
+          easing: Easing.linear,
+          useNativeDriver: true 
         }
       ).start(() => this.animate())
   }
